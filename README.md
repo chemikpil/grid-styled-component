e4be2d7c8362b7e46927c170f48b0b072147f089
Michal Mackowiak <chemikpil@gmail.com>
2020-10-26 19:48:15 +0100

    🤯 Create first version of grid styled component

--- README.md
+++ README.md
@@ -1,20 +1,89 @@
 # 🏗 Grid Styled Component
 
-An abstraction for CSS Grid Module with more friendly API. Grid Styled Component use emotion.js under the hood
+Abstraction for CSS Grid Module with more friendly API. Grid Styled Component use emotion.js under the hood
 
 ### 👨‍💻 Exemples
 
-tbd...
+#### Horizontal
+
+```HTML
+<Grid.Horizontal itemSize={200} justifyContent="space-between">
+    <div>Item 1</div>
+    <div>Item 2</div>
+    <div>Item 3</div>
+    <div>Item 4</div>
+<Grid.Horizontal>
+```
+
+#### Vertical
+
+```HTML
+<Grid.Vertical debug gap={20}>
+    <div>Item 1</div>
+    <div>Item 2</div>
+    <div>Item 3</div>
+    <div>Item 4</div>
+</Grid.Vertical>
+```
+
+#### Complex layout 2x2
+
+```HTML
+<Grid.Layout columns={3} rows={2} debug gap={20}>
+    <Grid.Item>Item 1</Grid.Item>
+    <Grid.Item>Item 2</Grid.Item>
+    <Grid.Item>Item 3</Grid.Item>
+    <Grid.Item>Item 4</Grid.Item>
+    <Grid.Item>Item 5</Grid.Item>
+    <Grid.Item>Item 6</Grid.Item>
+</Grid.Layout>
+```
+
+#### Responsive break
+
+```HTML
+<Grid.Horizontal
+    gap={30}
+    breakAt={800}
+    minCols={2}
+    maxCols={4}
+    debug
+>
+    <Grid.Item centerContent>Item 1</Grid.Item>
+    <Grid.Item>Item 2</Grid.Item>
+    <Grid.Item>Item 3</Grid.Item>
+    <Grid.Item>Item 4</Grid.Item>
+</Grid.Horizontal>
+```
+
+#### Multigap (API will change)
+
+```HTML
+<Grid.Vertical
+    debug
+    template="
+        [top-start] auto [top-end]
+        50px
+        [center-start] auto [center-end]
+        120px
+        [bottom-start] auto [bottom-end]
+    "
+>
+    <Grid.Item row="top-start / top-end">home</Grid.Item>
+    <Grid.Item row="center-start / center-end">logo</Grid.Item>
+    <Grid.Item row="bottom-start / bottom-end">menu</Grid.Item>
+</Grid.Vertical>
+```
 
 ### 🛠 API
 
-tdb
+tdb...
 
 ### ✅ TO DO
 
--   Add missing api to Grid adn Child component
+-   Add missing api to Grid and Child component (e.g. grid area)
 -   Add `min`, `max` and `clamp` to Grid component
--   Add `break` prop to make grid more responsive ([codepen](https://codepen.io/dannievinther/pen/b85a824d3f1afc6e400722ee5c411a12), [article](https://blog.logrocket.com/flexible-layouts-without-media-queries/))
+-   Try to implement multi-gap with better and simpler API
 
 ---
 
---
+++ src/grid-styled-component.ts
@@ -0,0 +1,213 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+type PlacementItems = 'start' | 'end' | 'center' | 'stretch';
+
+type PlacementContent =
+    | PlacementItems
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+
+type Children = React.ReactNode | React.ReactNode;
+
+interface GridProps {
+    units?: string;
+    itemSize?: string | number;
+    items?: number;
+    gap?: string | number;
+    gaps?: (string | number)[];
+    inline?: boolean;
+    template?: string;
+    fullWidth?: boolean;
+    fullHeight?: boolean;
+
+    justifyItems?: PlacementItems;
+    alignItems?: PlacementItems;
+    centerItems?: boolean;
+    justifyContent?: PlacementContent;
+    alignContent?: PlacementContent;
+
+    breakAt?: string | number;
+    minCols?: number;
+    maxCols?: number;
+
+    debug?: boolean;
+    as?: string;
+}
+
+interface GridLayoutProps extends GridProps {
+    columns?: number;
+    rows?: number;
+    columnSize?: number;
+    rowSize?: number;
+}
+
+type Template = {
+    items: number;
+    size: number | string;
+    units: string;
+};
+
+type Unit = {
+    value: number | string;
+    units: string;
+};
+
+interface GridItemProps {
+    column?: string;
+    row?: string;
+    justifySelf?: PlacementItems;
+    alignSelf?: PlacementItems;
+    centerSelf?: boolean;
+    centerContent?: boolean;
+}
+
+const getValueWithUnits = ({ value = 0, units = 'px' }: Unit) =>
+    typeof value === 'number' ? `${value}${units}` : value;
+
+const templateUtil = ({ items, size, units }: Template) => {
+    return `repeat(${items}, ${getValueWithUnits({
+        value: size,
+        units,
+    })})`;
+};
+
+const getChildCount = (children: Children) =>
+    Array.isArray(children) ? children.length : 1;
+
+const getCommonProps = ({ units = 'px', gap, ...props }: GridProps) => ({
+    display: props.inline ? 'inline-grid' : 'grid',
+    ...(props.fullWidth && { minWidth: '100%' }),
+    ...(props.fullHeight && { minHeight: '100%' }),
+    ...(props.debug && {
+        outline: '1px solid red',
+        '& > *': { outline: '1px dotted green' },
+    }),
+    ...(gap && {
+        gridGap: getValueWithUnits({ value: gap, units }),
+    }),
+    ...(props.justifyItems && { justifyItems: props.justifyItems }),
+    ...(props.alignItems && { alignItems: props.alignItems }),
+    ...(props.centerItems && {
+        justifyItems: 'center',
+        alignItems: 'center',
+    }),
+    ...(props.justifyContent && { justifyContent: props.justifyContent }),
+    ...(props.alignContent && { alignContent: props.alignContent }),
+});
+
+const getVerticalProps = ({
+    itemSize = '1fr',
+    units = 'px',
+    children,
+    ...props
+}: GridProps & { children: Children }) => {
+    const items = props.items || getChildCount(children);
+
+    return {
+        ...getCommonProps(props),
+        ...(items && {
+            gridTemplateRows: templateUtil({ items, size: itemSize, units }),
+        }),
+        ...(props.template && { gridTemplateRows: props.template }),
+        gridAutoFlow: 'row',
+    };
+};
+
+const getHorizontalProps = ({
+    itemSize = '1fr',
+    minCols = 2,
+    maxCols = 4,
+    children,
+    ...props
+}: GridProps & { children: Children }) => {
+    const items = props.items || getChildCount(children);
+    const gap = getValueWithUnits({
+        value: props.gap,
+        units: props.units,
+    });
+
+    return {
+        ...getCommonProps(props),
+        ...(items &&
+            !props.breakAt && {
+                gridTemplateColumns: templateUtil({
+                    items,
+                    size: itemSize,
+                    units: props.units,
+                }),
+            }),
+        ...(props.template && { gridTemplateColumns: props.template }),
+        gridAutoFlow: 'column',
+        ...(props.breakAt && {
+            gridAutoFlow: 'dense',
+            gridTemplateColumns: `repeat(
+                    auto-fill,
+                    minmax(
+                        clamp(
+                            100% / ${maxCols} - ${gap},
+                            (${getValueWithUnits({
+                                value: props.breakAt,
+                                units: props.units,
+                            })} - 100%) * 999,
+                            100% / ${minCols} - ${gap}
+                        ),
+                        1fr
+                    )
+                )`,
+        }),
+    };
+};
+
+const getLayoutProps = ({
+    itemSize = '1fr',
+    units = 'px',
+    ...props
+}: GridLayoutProps) => {
+    const columnSize = props.columnSize || itemSize;
+    const rowSize = props.rowSize || itemSize;
+
+    return {
+        ...getCommonProps(props),
+        ...(props.columns && {
+            gridTemplateColumns: templateUtil({
+                items: props.columns,
+                size: columnSize,
+                units,
+            }),
+        }),
+        ...(props.rows && {
+            gridTemplateRows: templateUtil({
+                items: props.rows,
+                size: rowSize,
+                units,
+            }),
+        }),
+        ...(props.template && { gridTemplate: props.template }),
+    };
+};
+
+const getItemProps = ({ ...props }: GridItemProps) => ({
+    ...(props.column && { gridColumn: props.column }),
+    ...(props.row && { gridRow: props.row }),
+    ...(props.justifySelf && { alignSelf: props.justifySelf }),
+    ...(props.alignSelf && { alignSelf: props.alignSelf }),
+    ...(props.centerSelf && { placeSelf: 'center' }),
+    ...(props.centerContent && {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }),
+});
+
+const Grid = {
+    Horizontal: styled.div<GridProps & { children: Children }>(
+        getHorizontalProps
+    ),
+    Vertical: styled.div<GridProps & { children: Children }>(getVerticalProps),
+    Layout: styled.div<GridLayoutProps>(getLayoutProps), // TO DO: pick a better name
+    Item: styled.div<GridItemProps>(getItemProps),
+};
+
+export { Grid };
